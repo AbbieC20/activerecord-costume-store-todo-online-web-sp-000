159ae83e3eb0b7fed7d551305885ef803a535a3a
remote-torrent-6747 <abbyc@sky.com>
2021-03-25 20:59:21 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,4 +2,16 @@
 # It should inherit from ActiveRecord::Base
 
 class Costume < ActiveRecord::Base
+
+  def change
+    create_table :costumes do |c|
+      c.string :name
+      c.integer :price
+      c.string :size
+      c.string :image_url
+      datetime :created_at
+      datetime :updated_at
+    end
+  end
+
 end
