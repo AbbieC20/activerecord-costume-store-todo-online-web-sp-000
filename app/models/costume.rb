c2488f623cce3cdb954ddd1eb6b5615a9125cb3b
remote-torrent-6747 <abbyc@sky.com>
2021-03-25 21:14:01 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,14 +1,2 @@
 class Costume < ActiveRecord::Base
-
-  def change
-    create_table :costumes do |c|
-      c.string :name
-      c.integer :price
-      c.string :size
-      c.string :image_url
-      c.datetime :created_at
-      c.datetime :updated_at
-    end
-  end
-
 end
